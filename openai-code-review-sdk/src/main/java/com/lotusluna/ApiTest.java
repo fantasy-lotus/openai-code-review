9a2d3fc86f801f68bf4ebd7761add9e2aadfa7fe
lotus <gilgamesh258@qq.com>
2024-12-14 20:17:08 +0800

    feat: init project

---
+++ openai-code-review-sdk/src/main/java/com/lotusluna/ApiTest.java
@@ -0,0 +1,10 @@
+package com.lotusluna;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ApiTest {
+    public static void main(String[] args) {
+        log.info("Hello, World!");
+    }
+}
